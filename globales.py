9707f0f4016a9327b404c46d2aecd296c9018bbd
Moibe <moi_estrello@hotmail.com>
2024-12-20 16:35:02 -0600

    Globales Ready

--- globales.py
+++ globales.py
@@ -1,5 +1,6 @@
 import gradio as gr
 
+<<<<<<< HEAD
 version = "1.1.2"
 env = "dev"
 aplicacion = "sampler-dev"
@@ -9,10 +10,18 @@ api_cost = "Moibe/sampler"
 same_api = True
 process_cost = 0
 
+=======
+version = "0.0.0"
+env = "dev"
+aplicacion = "sampler" #Se usa para que la api de sulku explore data y novelty de ese usuario.
+api = "Moibe/sampler"
+#api = "abidlabs/music-separation"
+#api = "https://abidlabs-music-separation.hf.space/--replicas/f5un4/"
+>>>>>>> e8b4771 (Globales Ready)
 seto = "sampler"
 work = "picswap"
 app_path = "/sampler-dev"
-server_port=7810
+server_port=7811
 #tema = tools.theme_selector()
 tema = gr.themes.Default()
 flag = "auto"
