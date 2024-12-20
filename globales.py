5def839d484a866f56940a40cc229b0c6623ba36
Moibe <moi_estrello@hotmail.com>
2024-12-20 17:02:54 -0600

    Sampler Right Path

--- globales.py
+++ globales.py
@@ -1,6 +1,7 @@
 import gradio as gr
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 version = "1.1.2"
 env = "dev"
 aplicacion = "sampler-dev"
@@ -12,6 +13,9 @@ process_cost = 0
 
 =======
 version = "0.0.0"
+=======
+version = "0.0.1"
+>>>>>>> 0e918aa (Sampler Right Path)
 env = "dev"
 aplicacion = "sampler" #Se usa para que la api de sulku explore data y novelty de ese usuario.
 api = "Moibe/sampler"
@@ -20,7 +24,7 @@ api = "Moibe/sampler"
 >>>>>>> e8b4771 (Globales Ready)
 seto = "sampler"
 work = "picswap"
-app_path = "/sampler-dev"
+app_path = "/sampler"
 server_port=7811
 #tema = tools.theme_selector()
 tema = gr.themes.Default()
